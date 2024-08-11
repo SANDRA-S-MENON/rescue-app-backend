6d5381fb54ae4f35be9485c857ad99dbb4968475
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-08-11 23:32:24 +0530

    admin SignUp created

--- app.js
+++ app.js
@@ -3,18 +3,35 @@ const Mongoose= require("mongoose")
 const Bcrypt= require("bcrypt")
 const Cors= require("cors")
 const jsonwebtoken= require("jsonwebtoken")
+const loginModel = require("./models/admin")
 
 
 
 
 let app =Express()
+app.use(Cors())
+app.use(Express.json())
 
 
+Mongoose.connect("mongodb+srv://sandras02:sandrasmenon@cluster0.3g103sn.mongodb.net/rescueappDb?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get("/",(res,req)=>{
-    res.send("hello")
 
+app.post("/adminSignUp",(req,res)=>{
+    let input=req.body
+    let hashedpassword=Bcrypt.hashSync(input.password,10)
+    input.password=hashedpassword
+    console.log(input)
+    let result=new loginModel(input)
+    result.save()
+    res.json({"status":"success"})
 })
+
+
+
+
+
+
+
 app.listen(5050,()=>{
     console.log("server started")
 })
